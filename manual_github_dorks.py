cc2c155e21a8ef49b3b23c493c18ffb3f68f173b
Saket Saurav <64626783+offsecin@users.noreply.github.com>
2020-08-12 10:58:02 +0530

    Update manual_github_dorks.py

--- manual_github_dorks.py
+++ manual_github_dorks.py
@@ -8,9 +8,21 @@ try:
 except:
     print("usage python3 github_dorks.py TARGET")
     sys.exit()
+
+
+
+common_dorks=["aws_keys",".bashrc","jira_creds","aws_keys","*.swp",".bashrc","ssh_keys","password","secret","credentials","token","config","key","pass","login","ftp","pwd","security_credentials","connectionstring","JDBC","ssh2_auth_password","send_keys","send,keys"]
+for cd in common_dorks:
+    print(requote_uri(url+"\"" +company+"\""+"  "+cd))
+
+
+
+
 filename='dorks.txt'
 with open(filename) as f:
     content = f.readlines()
     content = [x.strip() for x in content]
+    
+
     for dorks in content:
         print(requote_uri(url+company+"  "+dorks))
